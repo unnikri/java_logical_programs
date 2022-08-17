b8ab755b22d6e4765ac1baefbefac808ad0b2840
unnikri <58915839+unnikri@users.noreply.github.com>
2022-08-17 12:27:15 +0530

    Add files via upload

---
+++ bitlabs.java
@@ -0,0 +1,10 @@
+
+
+class bitlabs
+  {
+    public static void main (String [] args)
+    {
+      System.out.println ("WELCOME TO JAVA PROGRAMMING");
+      System.out.println ("WELCOME TO BITLABS");
+    }
+  }
