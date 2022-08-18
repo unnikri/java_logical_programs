260330d010923b12d1e9fb2a033c3ee9ea9204de
unnikri <58915839+unnikri@users.noreply.github.com>
2022-08-18 12:44:18 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,12 @@
+class area
+{
+  public static void main(String[] args)
+  {
+    int r=6;
+    double pi=3.14;
+    double ar;
+    System.out.println("The area of the circle is ");
+    ar=pi*r*r;
+      System.out.println(ar);
+  }
+}
